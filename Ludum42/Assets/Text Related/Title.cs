5d26781a66c3f57f6dac4471a5f966054a668f2a
Jesse Titus <jessejtitus@gmail.com>
2018-08-06 01:31:51 -0400

    Updated prologue

--- Ludum42/Assets/Text Related/Title.cs
+++ Ludum42/Assets/Text Related/Title.cs
@@ -8,6 +8,7 @@ public class Title : MonoBehaviour {
 
     public Text myText;          //actual text onscreen
     public List<string> prologue;   //a list of lines to feed the prologue before the main menu hits
+    public AudioSource audioPlayer;
 
     int currentLine = 0;    //current line the prologue is on
     Animator anim;      //to control which fade-in / fade-out if needed
@@ -16,6 +17,7 @@ public class Title : MonoBehaviour {
     
 	void Start () {
         anim = GetComponent<Animator>();
+        myText.text = prologue[0];
 	}
 	
 	public void NextLine()
@@ -29,5 +31,11 @@ public class Title : MonoBehaviour {
             //TODO - when prologue ends, show outline of cthulhu, and then title screen
             anim.SetTrigger("Reveal");
         }
+    }
+
+    public void PlaySound()
+    {
+        //Play lightning strike noise (on cthulhu reveal)
+        audioPlayer.Play();
     }
 }
