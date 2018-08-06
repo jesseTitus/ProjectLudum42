c620ee7cdbf2475d9bfefd78550671a35b1e20e5
Jesse Titus <jessejtitus@gmail.com>
2018-08-06 00:23:07 -0400

    Added prologue concept

---
+++ Ludum42/Assets/Title.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.UI;
+
+public class Title : MonoBehaviour {
+
+    public Text myText;          //actual text onscreen
+    public List<string> prologue;   //a list of lines to feed the prologue before the main menu hits
+
+    int currentLine = 0;    //current line the prologue is on
+    Animator anim;      //to control which fade-in / fade-out if needed
+
+
+    
+	void Start () {
+        anim = GetComponent<Animator>();
+	}
+	
+	public void NextLine()
+    {
+        if (++currentLine <= prologue.Count)
+        {
+            //move to the next line of the prologue to be displayed
+            myText.text = prologue[currentLine];
+        } else
+        {
+            //TODO - when prologue ends, show outline of cthulhu, and then title screen
+            anim.speed = 0;
+        }
+    }
+}
