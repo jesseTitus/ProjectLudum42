4f7972bbb9cb268a2ef7df2b78c865f647689015
Jesse Titus <jessejtitus@gmail.com>
2018-08-12 19:00:15 -0400

    Added Music and SoundFX manager

---
+++ Ludum42/Assets/Music.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Music : MonoBehaviour {
+    /// <summary>
+    /// This class manages music
+    /// It should switch between exploration | combat 
+    /// </summary>
+    public AudioClip explorationMusic;
+    public AudioClip combatMusic;
+
+    AudioSource musicPlayer;
+
+	void Start () {
+        musicPlayer = GetComponent<AudioSource>();
+        musicPlayer.clip = explorationMusic;
+    }
+
+    public void ChangeMusic(bool combat)
+    {
+        //TODO - fade IN fade OUT
+        if (combat)
+        {
+            musicPlayer.clip = combatMusic;
+        } else
+        {
+            musicPlayer.clip = explorationMusic;
+        }
+        musicPlayer.Play();
+    }
+    
+
+}
---
+++ Ludum42/Assets/SoundEffects.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundEffects : MonoBehaviour {
+    /// <summary>
+    /// This is the sound effects manager
+    /// It has a public method "PlaySoundEffect()"
+    /// which can be accessed by other classes to play the desired sound
+    /// </summary>
+    AudioSource soundFXManager;
+    public AudioClip[] sounds;
+
+	void Start () {
+        soundFXManager = GetComponent<AudioSource>();
+
+    }
+	
+	public void PlaySoundEffect(int effectNum)
+    {
+        if (effectNum < 0 || effectNum >= sounds.Length) return;
+
+        soundFXManager.clip = sounds[effectNum];
+        soundFXManager.Play();
+
+
+    }
+}
