a4b844eb8655aedc5d72cff0842c29efade20c83
Jesse Titus <jessejtitus@gmail.com>
2018-08-13 15:46:36 -0400

    Extras scene

---
+++ Ludum42/Assets/SlideShow.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlideShow : MonoBehaviour {
+
+    public Sprite[] cardSprites;
+
+    SpriteRenderer spriteRenderer;
+    int cardShown;
+
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    void OnMouseDown()
+    {
+        cardShown++;
+        if (cardShown >= cardSprites.Length) cardShown = 0;
+        spriteRenderer.sprite = cardSprites[cardShown];
+    }
+}
